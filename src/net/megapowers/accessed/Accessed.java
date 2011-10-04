558cdf9d289b223a7a79022ae930e96ef42693c0
phr0z3nt04st <phr0z3nt04st@gmail.com>
2011-10-03 22:23:41 -0500

    Add ability to allow access to certain IP addresses

--- src/net/megapowers/accessed/Accessed.java
+++ src/net/megapowers/accessed/Accessed.java
@@ -6,50 +6,69 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Accessed extends JavaPlugin {
 
-    AccessedThread reloadedThread;
+    ArrayList<AccessedThread> reloadedThreads;
+    static Config config = new Config();
 
     @Override
     public void onDisable() {
-        reloadedThread.running = false;
+        for (AccessedThread reloadedThread : reloadedThreads) {
+            reloadedThread.running = false;
+        }
         Logger.getLogger("Minecraft").info("[Accessed] Waiting 2 seconds for the socket to close");
         try {
             Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            Logger.getLogger("Minecraft").log(Level.SEVERE, null, ex);
         }
     }
 
     @Override
     public void onEnable() {
-        try{
-            reloadedThread = new AccessedThread(createServerSocket(InetAddress.getLocalHost(), 7040), this);
-            reloadedThread.start();
-        }catch(UnknownHostException e)
-        {
-            e.printStackTrace();
-            return;
+
+        List ips = (List) config.getConfig("ip-white-list");
+        AccessedThread reloadedThread = null;
+        for (Object ip : ips) {
+            String raw[] = ((String) ip).split("@");
+            Integer port = Integer.getInteger(raw[1], 7040);
+            try {
+                InetAddress addr = InetAddress.getByName(raw[0]);
+                reloadedThread = new AccessedThread(createServerSocket(addr, port), this);
+            } catch (UnknownHostException ex) {
+                Logger.getLogger("Minecraft").log(Level.SEVERE, null, ex);
+                continue;
+            } catch (IOException ex) {
+                Logger.getLogger("Minecraft").log(Level.SEVERE, null, ex);
+                continue;
+            }
+            reloadedThreads.add(reloadedThread);
+            Logger.getLogger("Minecraft").log(Level.INFO, "[Accessed] Enabled and listening on {0}@{1}", new Object[]{raw[0], port});
         }
-        Logger.getLogger("Minecraft").info("[Accessed] Enabled and listening on port 7040");
+
+        try {
+            reloadedThreads.add(new AccessedThread(createServerSocket(InetAddress.getLocalHost(), 7040), this));
+
+            Logger.getLogger("Minecraft").log(Level.INFO, "[Accessed] Enabled and listening on port {0}@7040", InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger("Minecraft").log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger("Minecraft").log(Level.SEVERE, null, ex);
+        }
     }
 
-    private ServerSocket createServerSocket(InetAddress addr, int port) {
+    private ServerSocket createServerSocket(InetAddress addr, int port) throws UnknownHostException, IOException {
         ServerSocket serverSocket = null;
-        try {
-            //local-link address
-            serverSocket = new ServerSocket();
-            SocketAddress sockaddr = new InetSocketAddress(addr, port);
-            serverSocket.bind(sockaddr);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        serverSocket = new ServerSocket();
+        SocketAddress sockaddr = new InetSocketAddress(addr, port);
+        serverSocket.bind(sockaddr);
         return serverSocket;
     }
 }
